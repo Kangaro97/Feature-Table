a40b5c8ad89bcd47aa94c30e386cea1cf0fc8254
Kangaro97 <kangaro97@gmail.com>
2020-09-24 15:44:38 +0300

    Updates

---
+++ precache-manifest.008c129010e19ee3651a6f690ccb75a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca885eb0e7ff8e077d10eaf7320bfe8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4548c5d9b8ee4e1fc809",
+    "url": "/static/css/main.84373846.chunk.css"
+  },
+  {
+    "revision": "7781e5f5d064c158e0b7",
+    "url": "/static/js/2.918cfc31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.918cfc31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4548c5d9b8ee4e1fc809",
+    "url": "/static/js/main.8609bca6.chunk.js"
+  },
+  {
+    "revision": "78499071eb0be4315710",
+    "url": "/static/js/runtime-main.01c9aeae.js"
+  }
+]);
